fe1f01836307a2e5dcf131f6298f9296d654793c
bertram gilfoyle <127828405+bertram-gilfoyle-233@users.noreply.github.com>
2023-04-21 11:58:20 +0800

    Create fn_interestness_template_gsa.py

---
+++ fn_interestness_template_gsa.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import shlex
+
+CSMITH_HEADER = "/usr/include/csmith"
+GCC_ANALYZER = "gcc -fanalyzer -fdiagnostics-plain-output -fdiagnostics-format=text"
+
+U_FLAG = 0
+N_FLAG = 0
+
+subprocess.run(["gcc", "-I", CSMITH_HEADER, "-O3", "-fsanitize=null", "example.c"],
+               stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+run_ret = subprocess.run(["timeout", "10s", "./a.out"],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+if run_ret.stderr.count("runtime error") < 1 and run_ret.stderr.count("null pointer") < 1:
+    exit(1)
+
+analyze_args_split = shlex.split(GCC_ANALYZER)
+analyze_ret = subprocess.run(
+    analyze_args_split + ["-I", CSMITH_HEADER, "-O3", "example.c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+if analyze_ret.stderr.count("error") != 0:
+    exit(5)
+if analyze_ret.stderr.count("data definition has no type or storage class") != 0:
+    exit(4)
+if analyze_ret.stderr.count("CWE-457") != 0:
+    exit(2)
+if analyze_ret.stderr.count("CWE-476") != 0:
+    exit(3)
+if analyze_ret.stderr.count("-Wimplicit-int") != 0:
+    exit(6)
+
+os.system(
+    "ccomp -I {} -interp -fall example.c > out_ccomp.txt 2>&1".format(CSMITH_HEADER))
+
+with open("out_ccomp.txt", "r") as f:
+    for line in f.readlines():
+        if "Undefined behavior" in line:
+            U_FLAG = 1
+        if "*0LL" in line:
+            N_FLAG = 1
+
+if U_FLAG == 1 and N_FLAG != 1:
+    exit(7)
+
+exit(0)
