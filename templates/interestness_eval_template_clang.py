715e5b74d2a821680afc79f04d182e016d6e432a
ML.M <dale.mengli.ming@proton.me>
2024-02-28 12:20:39 +0800

    remove alpha.unix.cstring.BufferOverlap

--- templates/interestness_eval_template_clang.py
+++ templates/interestness_eval_template_clang.py
@@ -60,12 +60,10 @@ if analyzer_ret.stderr.count("alpha.core.StackAddressAsyncEscape") != 0:
     exit(17)
 if analyzer_ret.stderr.count("alpha.security.ArrayBoundV2") != 0:
     exit(18)
-if analyzer_ret.stderr.count("alpha.unix.cstring.BufferOverlap") != 0:
-    exit(19)
 if analyzer_ret.stderr.count("alpha.unix.cstring.OutOfBounds") != 0:
-    exit(20)
+    exit(19)
 if analyzer_ret.stderr.count("alpha.unix.cstring.UninitializedRead") != 0:
-    exit(21)
+    exit(20)
 
 # use ccomp to check if there are undefined behaviors
 print("run compcert")
@@ -74,7 +72,7 @@ ccomp_ret = subprocess.run(['ccomp', '-I', CSMITH_HEADER, '-interp', '-fall', '-
 if ccomp_ret.stdout.count("Undefined behavior") != 0:
     print("Undefined behavior")
     # print(ccomp_ret)
-    exit(22)
+    exit(21)
 if ccomp_ret.returncode != 0:
     print("ccomp_ret returncode: %s" % ccomp_ret.returncode)
     print("Compcert failed")  # cannot comment this line!
